05cfa4866a22f9063d0cf20ea59175c0e3fb7c0a
Guilherme Michelan (Tacobarry) <tacobarry@gmail.com>
2021-11-07 00:34:47 -0300

    recebendo stream de dados

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/tacobarry/fc2-grpc/pb"
@@ -17,7 +18,8 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	AddUserVerbose(client)
+	// AddUser(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -33,4 +35,29 @@ func AddUser(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Joaozinho",
+		Email: "jao.teixeira@sensedia.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("could not make gRPC reques stream: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receive the msg: %v", err)
+		}
+		fmt.Println("Status:", stream.Status)
+	}
 }
